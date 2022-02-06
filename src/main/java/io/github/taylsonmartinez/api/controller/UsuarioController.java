58e915b9f54052010e8a99f8a454645d296c91ba
taylsonmartinez <taylson.martinez@gmail.com>
2022-02-06 10:26:11 -0300

    Usuario Service

---
+++ src/main/java/io/github/taylsonmartinez/api/controller/UsuarioController.java
@@ -0,0 +1,27 @@
+package io.github.taylsonmartinez.api.controller;
+
+import io.github.taylsonmartinez.entity.Usuario;
+import io.github.taylsonmartinez.service.impl.UsuarioServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioServiceImpl usuarioService;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Usuario salvar( @RequestBody @Valid Usuario usuario ){
+        String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(senhaCriptografada);
+        return usuarioService.salvar(usuario);
+    }
+}
---
+++ src/main/java/io/github/taylsonmartinez/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package io.github.taylsonmartinez.repository;
+
+import io.github.taylsonmartinez.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByLogin(String login);
+}
