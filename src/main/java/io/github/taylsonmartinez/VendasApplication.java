62800697e00105bbbbc8916a0594ef3e3012175a
taylsonmartinez <taylson.martinez@gmail.com>
2022-02-06 10:45:48 -0300

    update SecurityConfig
    api/usuarios

--- src/main/java/io/github/taylsonmartinez/VendasApplication.java
+++ src/main/java/io/github/taylsonmartinez/VendasApplication.java
@@ -11,14 +11,6 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class VendasApplication {
 
-    @Bean
-    public CommandLineRunner commandLineRunner(@Autowired ClientesRepository clientesRepository){
-        return args -> {
-            Cliente c = new Cliente(null, "Fulano", "42246924065");
-            clientesRepository.save(c);
-        };
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
