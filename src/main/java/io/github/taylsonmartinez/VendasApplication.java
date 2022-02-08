bf83eb6abb5f7d50af2c65acea1826927cecc9ed
taylsonmartinez <taylson.martinez@gmail.com>
2022-02-08 00:01:21 -0300

    Org imports

--- src/main/java/io/github/taylsonmartinez/VendasApplication.java
+++ src/main/java/io/github/taylsonmartinez/VendasApplication.java
@@ -1,12 +1,7 @@
 package io.github.taylsonmartinez;
 
-import io.github.taylsonmartinez.entity.Cliente;
-import io.github.taylsonmartinez.repository.ClientesRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
